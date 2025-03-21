33422079f9d2ba03b0e644abb08eb7d87fca877b
RichyJohanness <riickyjohaness@gmail.com>
2025-03-22 05:21:13 +0700

    k

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,22 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./",  // Ubah base agar tidak terjadi masalah path
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // 📝 Base URL untuk deploy ke GitHub Pages
-  base: '/website-febri/', // Ganti sesuai dengan nama repository GitHub Anda
 }));
